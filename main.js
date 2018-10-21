653cea6a94adf9b6f83b5480771db58c59c56fb6
Taylor Leach <taylorleach@hotmail.com>
2018-10-21 13:41:57 -0700

    adjusted window dimensions and instructions

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const electron = require('electron');
-const storage = require('electron-json-storage');
 
 // Module to control application life.
 const app = electron.app;
@@ -13,10 +12,12 @@ const url = require('url');
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
-
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 800, height: 700 });
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 760
+  });
 
   // and load the index.html of the app.
   mainWindow.loadURL(
@@ -25,14 +26,13 @@ function createWindow() {
       protocol: 'file:',
       slashes: true
     })
-
   );
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -46,7 +46,7 @@ function createWindow() {
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
@@ -54,7 +54,7 @@ app.on('window-all-closed', function() {
   }
 });
 
-app.on('activate', function() {
+app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
